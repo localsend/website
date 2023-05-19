b4dbff931218b108c87aad756cb57940adde57be
TheGB0077 <39922116+TheGB0077@users.noreply.github.com>
2023-05-19 18:04:23 -0300

    i18n: add pt-BR

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -2,6 +2,7 @@ import { createI18n } from 'vue-i18n'
 import de from '@/locales/de.json'
 import en from '@/locales/en.json'
 import it from '@/locales/it.json'
+import ptBR from '@/locales/pt-BR.json'
 import zhCN from '@/locales/zh-CN.json'
 import zhTW from '@/locales/zh-TW.json'
 
@@ -9,6 +10,7 @@ export const locales: {[key: string]: [string, Record<string, any>]} = {
   'de': ['Deutsch', de],
   'en': ['English', en],
   'it': ['Italiano', it],
+  'pt-BR': ['Português brasileiro', ptBR],
   'zh-CN': ['简体中文', zhCN],
   'zh-TW': ['繁體中文', zhTW],
 };
