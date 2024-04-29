818f0c250044d1a2097c7af60f1f84519f0494cb
Tien Do Nam <dev.tien.donam@gmail.com>
2024-04-29 17:54:35 +0200

    feat: improve home layout

--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,8 @@
   <div class="min-h-screen flex items-center justify-center pt-12 md:pt-0">
     <div style="max-width: 1800px" class="grid grid-cols-12">
       <!-- Logo and text -->
-      <div class="col-span-12 sm:col-span-12 md:col-span-8 lg:col-span-7 grid grid-cols-12">
-        <div class="col-span-12 md:col-span-4 flex items-center justify-center md:justify-end">
+      <div class="col-span-12 sm:col-span-12 md:col-span-8 lg:col-span-7 pb-12 md:pb-0 grid grid-cols-12">
+        <div class="col-span-12 md:col-span-4 flex items-center justify-center md:justify-end mt-0 lg:mt-2">
           <img
               id="logo"
               src="~/assets/img/logo-512.png" alt="LocalSend Logo"
@@ -11,11 +11,11 @@
           />
         </div>
 
-        <div class="col-span-12 md:col-span-8 flex items-center justify-center md:justify-start pb-12 md:pb-0 lg:pl-2">
+        <div class="col-span-12 md:col-span-8 flex items-center justify-center md:justify-start lg:pl-2">
           <div>
             <h1 class="text-5xl sm:text-5xl xl:text-7xl 2xl:text-8xl font-black text-center md:text-start">LocalSend</h1>
-            <h2 class="text-xl xl:text-2xl 2xl:text-3xl font-light text-center md:text-start mt-2">{{ t('home.slogan1') }}</h2>
-            <h2 class="text-xl xl:text-2xl 2xl:text-3xl font-light text-center md:text-start">{{ t('home.slogan2') }}</h2>
+            <h2 class="text-xl md:text-xl xl:text-2xl 2xl:text-3xl font-light text-center md:text-start mt-2">{{ t('home.slogan1') }}</h2>
+            <h2 class="text-xl md:text-xl xl:text-2xl 2xl:text-3xl font-light text-center md:text-start">{{ t('home.slogan2') }}</h2>
 
             <div class="mt-6 text-center md:text-start">
               <NuxtLink :to="localePath({ path: '/download' })">
@@ -34,13 +34,13 @@
       </div>
 
       <!-- Screenshots -->
-      <div class="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-5 grid grid-cols-2">
-        <div class="col-span-2 md:col-span-1">
+      <div class="col-span-12 sm:col-span-12 md:col-span-4 lg:col-span-5 grid grid-cols-3 gap-8 px-8">
+        <div class="col-span-3 md:col-span-1">
           <img src="/img/screenshot-iphone.webp" alt="iPhone Screenshot"
                :style="{height: screenshotHeight + 'px', margin: 'auto'}"
                class="object-contain"/>
         </div>
-        <div class="col-span-2 md:col-span-1">
+        <div class="col-span-3 md:col-span-2">
           <img src="/img/screenshot-pc.webp" alt="PC Screenshot"
                :style="{height: screenshotHeight + 'px', margin: 'auto'}" class="object-contain"/>
         </div>
@@ -62,20 +62,25 @@ const localePath = useLocalePath()
 
 const breakpoints = useBreakpoints(breakpointsTailwind);
 
+const is2Xl = breakpoints.greaterOrEqual('2xl');
+const isXl = breakpoints.greaterOrEqual('xl');
+const isLg = breakpoints.greaterOrEqual('lg');
+const isMd = breakpoints.greaterOrEqual('md');
+
 const logoHeight = computed(() => {
-  if (breakpoints.greaterOrEqual('2xl')) {
+  if (is2Xl.value) {
+    return 220;
+  }
+
+  if (isXl.value) {
     return 200;
   }
 
-  if (breakpoints.greaterOrEqual('xl')) {
-    return 200;
+  if (isLg.value) {
+    return 150;
   }
 
-  if (breakpoints.greaterOrEqual('lg')) {
-    return 200;
-  }
-
-  if (breakpoints.greaterOrEqual('md')) {
+  if (isMd.value) {
     return 150;
   }
 
@@ -83,23 +88,23 @@ const logoHeight = computed(() => {
 });
 
 const screenshotHeight = computed(() => {
-  if (breakpoints.greaterOrEqual('2xl')) {
-    return 400;
+  if (is2Xl.value) {
+    return 500;
   }
 
-  if (breakpoints.greaterOrEqual('xl')) {
+  if (isXl.value) {
+    return 250;
+  }
+
+  if (isLg.value) {
     return 200;
   }
 
-  if (breakpoints.greaterOrEqual('lg')) {
+  if (isMd.value) {
     return 150;
   }
 
-  if (breakpoints.greaterOrEqual('md')) {
-    return 150;
-  }
-
-  return 100;
+  return 400;
 });
 
 onMounted(() => {
