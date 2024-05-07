c86a494c83767c59f1e12af32da6a8fcf0de8063
Tien Do Nam <dev.tien.donam@gmail.com>
2024-05-08 00:29:59 +0200

    feat: change h2 to h3

--- components/FeatureCard.vue
+++ components/FeatureCard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col items-center bg-gray-100 rounded-xl pb-4 pt-8 pl-4 pr-4">
     <Icon :name="props.icon" class="text-6xl" />
-    <h2 class="text-xl font-bold text-center mt-4">{{ props.title }}</h2>
+    <h3 class="text-xl font-bold text-center mt-4">{{ props.title }}</h3>
     <p class="text-center mt-2">{{ props.description }}</p>
   </div>
 </template>
