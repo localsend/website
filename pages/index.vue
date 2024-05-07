9e9cd122b0f17f02f611cc3054083f8e237c2dc0
Tien Do Nam <dev.tien.donam@gmail.com>
2024-05-08 01:57:35 +0200

    feat: add mentioned section

--- pages/index.vue
+++ pages/index.vue
@@ -53,6 +53,9 @@
         </div>
       </div>
     </div>
+
+    <!-- End of hero section -->
+
     <div class="m-auto max-w-screen-lg flex flex-col items-center mt-4 px-4 lg:px-12">
       <h2 class="text-3xl font-bold">{{ t('home.features.title') }}</h2>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 lg:gap-12 mt-8">
@@ -76,6 +79,46 @@
                      icon="material-symbols:gesture"/>
       </div>
 
+      <h2 class="text-3xl font-bold mt-24">{{ t('home.mentioned') }}</h2>
+      <InfiniteHorizontalScroll class="w-full mt-8 [&_a]:mx-4 [&_img]:max-w-none">
+        <MentionedCard title="Use This App to Easily Send Files to Any Device"
+                       description="Lifehacker"
+                       language="twemoji:flag-united-states"
+                       url="https://lifehacker.com/tech/use-localsend-app-to-send-files-across-devices"/>
+        <MentionedCard title="Freeware LocalSend: Wie AirDrop für alle Plattformen"
+                       description="ifun"
+                       language="twemoji:flag-germany"
+                       url="https://www.ifun.de/freeware-localsend-wie-airdrop-fuer-alle-plattformen-220789/"/>
+        <MentionedCard title="LocalSend – 无联网，开源跨平台的局域网文件互传工具[2023年的第一个精选]"
+                       description="APPINN"
+                       language="twemoji:flag-china"
+                       url="https://www.appinn.com/localsend/"/>
+        <MentionedCard title="Cette application universelle offre un partage de fichiers inspiré d’AirDrop à tous vos appareils"
+                       description="01net"
+                       language="twemoji:flag-france"
+                       url="https://www.01net.com/astuces/cette-application-universelle-offre-un-partage-de-fichiers-inspire-dairdrop-a-tous-vos-appareils.html"/>
+        <MentionedCard title="クロスプラットフォームに対応したファイル共有ソフト！「LocalSend」。"
+                       description="gigafree.net"
+                       language="twemoji:flag-japan"
+                       url="https://www.gigafree.net/internet/share/LocalSend.html"/>
+        <MentionedCard title="LocalSend: Transfiere archivos del móvil al ordenador, y viceversa"
+                       description="Neoteo"
+                       language="twemoji:flag-spain"
+                       url="https://www.neoteo.com/localsend-transfiere-archivos-del-movil-al-ordenador-y-viceversa/"/>
+        <MentionedCard title="LocalSend — бесплатная программа для обмена файлами и текстом через WiFi"
+                       description="alexandertokarev.ru"
+                       language="twemoji:flag-russia"
+                       url="https://alexandertokarev.ru/all/localsend-besplatnaya-programma-dlya-obmena-faylami-cherez-wifi/"/>
+        <MentionedCard title="대용량 파일 전송 끝판왕 크로스플랫폼 LocalSend"
+                       description="naver.com"
+                       language="twemoji:flag-south-korea"
+                       url="https://m.blog.naver.com/vavaras/223352646162"/>
+        <MentionedCard title="Invia file in modo sicuro sulla rete locale con l’app LocalSend multipiattaforma"
+                       description="Guidetti Informatica"
+                       language="twemoji:flag-italy"
+                       url="https://www.guidetti-informatica.net/2023/03/invia-file-in-modo-sicuro-sulla-rete-locale-con-lapp-localsend-multipiattaforma/"/>
+      </InfiniteHorizontalScroll>
+
       <h2 class="text-3xl font-bold mt-24">{{ t('home.get') }}</h2>
       <div class="mt-8 mb-48 flex items-center justify-center gap-4">
         <NuxtLink :to="localePath({ path: '/download' })">
@@ -85,7 +128,8 @@
         </NuxtLink>
       </div>
 
-      <TextButton href="https://github.com/localsend/website" target="_blank" icon="material-symbols:build" class="mb-8">
+      <TextButton href="https://github.com/localsend/website" target="_blank" icon="material-symbols:build"
+                  class="mb-8">
         {{ t('improveWebsite') }}
       </TextButton>
     </div>
