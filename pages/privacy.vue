6a01740b14f234930d198f638c7770ea8e78f9df
Tien Do Nam <dev.tien.donam@gmail.com>
2024-05-08 20:47:39 +0200

    docs: update privacy policy

--- pages/privacy.vue
+++ pages/privacy.vue
@@ -1,29 +1,66 @@
 <template>
   <LegalLayout>
     <h1>Privacy Policy</h1>
-    <p>Last updated: May 14, 2023</p>
+    <p>Last updated: May 8, 2024</p>
     <br>
 
     <h2>Introduction</h2>
-    <p>This Privacy Policy is meant to help you understand our practices regarding any information we might collect from you or that you provide to us, the ways in which we use this information, and how we handle it.</p>
-    <p>Given that we do not collect any personal data, our practices are straightforward and committed to safeguarding your privacy.</p>
+    <p>
+      This Privacy Policy is designed to help you understand our practices regarding any information we might collect
+      from you or that you provide to us, the ways in which we use this information, and how we handle it.<br>
+      Given that we do not collect any personal data or non-personal data, our practices are straightforward and
+      committed
+      to safeguarding your privacy.
+    </p>
+
     <br>
 
     <h2>Personal Data Collection and Use</h2>
-    <p>LocalSend is an open-source application, and we are dedicated to user privacy. In line with this, we confirm that we do not collect, store, or process any personal data from you while you use our application.</p>
-    <p>Personal data refers to any information that could potentially identify you as an individual. Since we do not collect such information, there's no possibility of us using, sharing, or selling this data.</p>
+    <p>
+      LocalSend is an open-source application, and we are dedicated to user privacy. In line with this, we confirm that
+      we do not collect, store, process, or use any personal data or non-personal data from you while you use our
+      application.
+      <br>
+      Personal data refers to any information that could potentially identify you as an individual. Non-personal data
+      refers to any information that does not directly identify you and is aggregated or anonymized. Since we do not
+      collect any such information, there's no possibility of us using, sharing, or selling this data.
+    </p>
+
     <br>
 
     <h2>Security</h2>
-    <p>While LocalSend does not collect personal data, we value your trust in providing any non-personal data, thus we strive to use acceptable means of protecting it. However, no method of transmission over the internet, or method of electronic storage is 100% secure and reliable, and we cannot guarantee its absolute security.</p>
+    <p>
+      As LocalSend does not collect any data, there are no concerns regarding the transmission or storage of data.
+      However, we still prioritize the security of our application and the trust you place in us by using it.
+    </p>
+
+    <br>
+
+    <h2>Third-Party Data Collection</h2>
+    <p>
+      While LocalSend itself does not collect any personal or non-personal data, it is important to note that users may
+      still be subject to data collection by third parties such as operating systems (e.g. Android, iOS), device
+      manufacturers, and other apps that have permissions to access device data. We have no control over and assume no
+      responsibility for the data practices of these third parties. We encourage users to review the privacy policies of
+      their operating system and device manufacturer to better understand their data practices.
+    </p>
+
     <br>
 
     <h2>Changes to this Privacy Policy</h2>
-    <p>We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.</p>
+    <p>
+      We may update our Privacy Policy from time to time. Thus, you are advised to review this page periodically for any
+      changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are
+      effective immediately after they are posted on this page.
+    </p>
+
     <br>
 
     <h2>Contact Us</h2>
-    <p>If you have any questions about this Privacy Policy, do not hesitate to contact us by email: support@localsend.org</p>
+    <p>
+      If you have any questions about this Privacy Policy, do not hesitate to contact us by email:
+      support@localsend.org
+    </p>
   </LegalLayout>
 </template>
 
