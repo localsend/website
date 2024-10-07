db1d3e8751f5cc0ae0c8b59479859a997c4b42f6
Michael Obubelebra Amachree <amachree9630@gmail.com>
2024-10-07 07:39:10 +0100

    ✨ feat ✨(ui): Add comprehensive dropdown menu components
    - Introduce a new dropdown menu component using Radix Vue
    - Add subcomponents for enhanced UI functionality:
      - Dropdown menu trigger, content, and item components
      - Checkbox item, radio group, and radio item components
      - Menu group, label, separator, and shortcut components
      - Subcontent, sub-item, and sub-trigger components
    - Implement customizable properties and styling for all components
    - Enhance button component with variant and size options
    - Add utility function to merge Tailwind CSS class names
    - Include Tailwind CSS config 🔧 with custom animations and dark mode
    - Integrate shadcn-nuxt module in Nuxt configuration
    - Update package 📦.json with necessary dependencies 📦
    
    These changes provide a robust and flexible UI component set for building
    dynamic and customizable dropdown menus, improving the overall user
    experience and design 🎨 consistency across the project.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ export default defineNuxtConfig({
     "@nuxtjs/i18n",
     "nuxt-icon",
     "@nuxtjs/sitemap",
+    "shadcn-nuxt",
   ],
   devtools: { enabled: true },
   app: {
---
+++ tailwind.config.js
@@ -0,0 +1,39 @@
+const animate = require("tailwindcss-animate")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    './pages/**/*.{ts,tsx,vue}',
+    './components/**/*.{ts,tsx,vue}',
+    './app/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
+	],
+  prefix: "",
+  theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
+    extend: {
+      keyframes: {
+        "accordion-down": {
+          from: { height: 0 },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
+    },
+  },
+  plugins: [animate],
+}
