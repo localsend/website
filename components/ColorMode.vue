2cb4000bdc5a75bb04f797c5c19a0341e2493c1b
Tien Do Nam <dev.tien.donam@gmail.com>
2024-10-07 16:08:15 +0200

    fix: make color mode client only

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="absolute right-32 top-5">
-    <button class="text-black dark:text-white" @click="next()">
+    <ClientOnly>
+      <button class="text-black dark:text-white" @click="next()">
       <span class="flex w-fit items-center space-x-2">
         <Icon
-          v-if="mode == 'light'"
-          name="material-symbols:light-mode-outline"
-          class="size-5 text-black dark:text-white"
-        />
-        <Icon v-if="mode == 'dark'"
-            name="material-symbols:dark-mode-outline"
+            :name="mode == 'light' ? 'material-symbols:light-mode-outline' : 'material-symbols:dark-mode-outline'"
             class="size-5 text-black dark:text-white"
         />
         <span class="sr-only"> {{ mode }} mode </span>
       </span>
-    </button>
+      </button>
+    </ClientOnly>
   </div>
 </template>
 
