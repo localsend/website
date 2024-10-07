cfcb92f015c99dcfde1909a93bd31f8d6918a88f
Tien Do Nam <dev.tien.donam@gmail.com>
2024-10-07 15:54:48 +0200

    refactor: simplify color mode

--- components/ColorMode.vue
+++ components/ColorMode.vue
@@ -1,41 +1,30 @@
 <template>
   <div class="absolute right-32 top-5">
-    <UseColorMode v-model="selectedMode">
-      <button class="text-black dark:text-white" @click="next()">
-        <span class="flex w-fit items-center space-x-2">
-          <Moon
-            v-if="mode === 'dark'"
+    <button class="text-black dark:text-white" @click="next()">
+      <span class="flex w-fit items-center space-x-2">
+        <Icon
+          v-if="mode == 'light'"
+          name="material-symbols:light-mode-outline"
+          class="size-5 text-black dark:text-white"
+        />
+        <Icon v-if="mode == 'dark'"
+            name="material-symbols:dark-mode-outline"
             class="size-5 text-black dark:text-white"
-          />
-          <Sun
-            v-if="mode === 'light'"
-            class="size-5 text-black dark:text-white"
-          />
-          <span class="sr-only"> {{ mode }} mode </span>
-        </span>
-      </button>
-    </UseColorMode>
+        />
+        <span class="sr-only"> {{ mode }} mode </span>
+      </span>
+    </button>
   </div>
 </template>
 
 <script setup lang="ts">
 import { useColorMode, useCycleList } from "@vueuse/core";
-import { watchEffect } from "vue-demi";
-import { Sun, Moon } from "lucide-vue-next";
 
-const mode = useColorMode({
-  emitAuto: false,
-});
-
-const selectedMode = ref(mode.value);
-
-const updateMode = () => {
-  mode.value = selectedMode.value;
-};
+const mode = useColorMode();
 
 const { state, next } = useCycleList(["dark", "light"] as const, {
   initialValue: mode,
 });
 
-watchEffect(() => (mode.value = state.value));
+watch(state, () => mode.value = state.value);
 </script>
