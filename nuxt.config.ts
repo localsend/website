6fc003d6c49247bc7e73982bf5829135f7691e93
nidexingg <gdarademoire2003@gmail.com>
2024-12-13 09:51:02 +0700

    chore: upgrade Nuxt to version 3.14.1592; feat: add Nuxt future compatibility for v4 and migrate to new folder structure; fix: address duplication of i18n.config.ts files in app and root directories, both files are currently necessary and can't be deleted either one of them, so the assistance in resolving this duplication would be appreciated.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  future: {
+    compatibilityVersion: 4,
+  },
   modules: [
     "@nuxtjs/tailwindcss",
     "@nuxtjs/i18n",
