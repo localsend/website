9cac80b724f03a5892ec7c8d22d5b5f625bea89d
David <David.aa.leren@gmail.com>
2025-10-30 15:14:32 +0100

    Add full Norwegian (Bokmål) translation for website content (#160)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -230,6 +230,13 @@ export default defineNuxtConfig({
         englishName: "Dutch",
       },
       {
+        code: "no",
+        language: "nb-NO",
+        file: "no.json",
+        name: "Norsk",
+        englishName: "Norwegian",
+      },
+      {
         code: "oc",
         language: "oc-FR",
         file: "oc.json",
