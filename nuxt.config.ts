0c00d9fa35a4776ee54c5358bf25451e42eea205
Tien Do Nam <dev.tien.donam@gmail.com>
2026-05-19 22:25:01 +0200

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,12 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/icon',
+    '@nuxtjs/tailwindcss',
+    '@nuxtjs/sitemap',
+    '@nuxtjs/i18n'
+  ]
+})
